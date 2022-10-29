bc02d763da1508e11ab0c1bdfb746d5e52a71909
deviant <mb.deviant@gmail.com>
2022-10-30 01:58:52 +0300

    edit submit button function

--- script.js
+++ script.js
@@ -3,15 +3,11 @@ const passwordConfirmation = document.getElementById('password-confirmation');
 const submitButton = document.getElementById("submit-button");
 const form = document.getElementById('sign-up');
 
-
-let passwordMatch = true;
-
 submitButton.addEventListener('click', () =>{
     if(passwordMatch == false){
         return;
     }
 })
-
 
 form.addEventListener('mouseleave', () => {
     checkPassword();
@@ -20,9 +16,12 @@ form.addEventListener('mouseleave', () => {
 function checkPassword() {
     if (passwordInput.value != passwordConfirmation.value) {
         submitButton.disabled = true;
+        submitButton.classList.add('disabled');
+        submitButton.classList.remove('enabled');
     }
     else if (passwordInput.value == passwordConfirmation.value){
-        
         submitButton.disabled = false;
+        submitButton.classList.add('enabled');
+        submitButton.classList.remove('disabled');
     }
 }
