d2ac792482c157054ee2cab9329bb27f27e55110
deviant <mb.deviant@gmail.com>
2022-10-31 03:33:56 +0300

    add second control to password match confirmation

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const passwordInput = document.getElementById('password');
 const passwordConfirmation = document.getElementById('password-confirmation');
 const submitButton = document.getElementById("submit-button");
-const form = document.getElementById('form-container');
+const formContainer = document.getElementById('form-container');
+const passwordMatchWarning = document.createElement('p');
 
-form.addEventListener('mousemove', () => {
-    checkPassword();
+passwordMatchWarning.textContent = "*Passwords does not match"
+
+formContainer.addEventListener('mousemove', () => {
+    if(passwordConfirmation.value != '') checkPassword();   
 });
+submitButton.addEventListener('mousedown', ()=>{
+    checkPassword();
+})
 
 function checkPassword() {
     if (passwordInput.value != passwordConfirmation.value) {
         submitButton.disabled = true;
         submitButton.classList.add('disabled');
         submitButton.classList.remove('enabled');
+        passwordConfirmation.appendChild(passwordMatchWarning);
 
     }
     else if (passwordInput.value == passwordConfirmation.value) {
